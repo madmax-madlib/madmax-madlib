72eb2d9167dd7e68c192a7c703429713c3bd9ce2
ikkanlaz <andrew.lazenby@gmail.com>
2017-08-23 19:22:43 -0700

    adding images to stories.js

--- models/stories.js
+++ models/stories.js
@@ -9,10 +9,9 @@ module.exports = function (sequelize, DataTypes) {
 
         storyDesc: {
             type: DataTypes.STRING,
-            allowNull: false        
+            allowNull: false
         },
-
-        imageUrls: {
+        posterImage: {
             type: DataTypes.STRING,
             allowNull: false
         },
@@ -24,22 +23,18 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: true
         },
-
         segment3: {
             type: DataTypes.STRING,
             allowNull: true
         },
-
         segment4: {
             type: DataTypes.STRING,
             allowNull: true
         },
-
         segment5: {
             type: DataTypes.STRING,
             allowNull: true
         },
-
         segment6: {
             type: DataTypes.STRING,
             allowNull: true
@@ -49,23 +44,58 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: true
         },
-
         segment8: {
             type: DataTypes.STRING,
             allowNull: true
         },
-
         segment9: {
             type: DataTypes.STRING,
             allowNull: true
         },
-
         segment10: {
             type: DataTypes.STRING,
             allowNull: true
         },
-
-
+        imageSegment1: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        imageSegment2: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        imageSegment3: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        imageSegment4: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        imageSegment5: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        imageSegment6: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        imageSegment7: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        imageSegment8: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        imageSegment9: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        imageSegment10: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
     });
 
     return Stories;
