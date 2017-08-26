684fdb8bd8ef5dd2099d6e3ef9ba349c3bc911bd
ikkanlaz <andrew.lazenby@gmail.com>
2017-08-25 21:55:44 -0700

    removing script file and adding vaidation script to main

--- public/assets/javascript/script.js
+++
@@ -1,5 +0,0 @@
-// during on click event, the text-to-speech button reads the rendered segment
-$( "#AlexisButton" ).click(function() {
-  responsiveVoice.speak( //pass here the variable of rendered segments
-  	);
-});
